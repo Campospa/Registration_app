aa049f32c232eaad03de38a451c786532f9f0dd5
Campospa <95729370+Campospa@users.noreply.github.com>
2024-06-26 19:25:59 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+
 from flask import Flask, request, render_template, redirect, url_for, flash
 from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship,session
 import datetime 
 import os
 
@@ -25,8 +26,9 @@ class Registration(Base):
     department = Column(String)
     position = Column(String)
     hire_date = Column(DateTime, default=datetime.datetime.utcnow)
+    status = Column(String, default='active')
+ 
     
-
     # Define foreign key to employees table
     employee_id = Column(Integer, ForeignKey('employees.id'))
     employee = relationship("Employees", back_populates="registration")
@@ -38,13 +40,14 @@ class Employees(Base):
     first_name = Column(String)
     last_name = Column(String)
     email = Column(String, unique=True)
+    phone = Column(String, unique=True)
+    date_of_birth = Column(DateTime)
+    gender = Column(String)
     
 
     registration = relationship("Registration", back_populates="employee")
 
-    def __str__(self):
-        return f"Employee_ID: {self.employee_id}, First Name: {self.first_name}, Last Name: {self.last_name}, Email: {self.email}, Job Title: {self.job_title}, Department: {self.department}, Hire Date: {self.hire_date}"
-
+    
 Base.metadata.create_all(engine)
 
 
@@ -60,12 +63,21 @@ def register_employee():
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
+        phone = request.form['phone']
+        date_of_birth_str = request.form['date_of_birth']
+        date_of_birth = datetime.datetime.strptime(date_of_birth_str, '%Y-%m-%d')
+        gender = request.form['gender']
+
+
         department = request.form['department']
         position = request.form['position']
         hire_date_str = request.form['hire_date']
         hire_date = datetime.datetime.strptime(hire_date_str, '%Y-%m-%d')
+        status = request.form['status']
         
-        new_employee = Employees(first_name=first_name, last_name=last_name, email=email)
+
+        new_employee = Employees(first_name=first_name, last_name=last_name, email=email, phone=phone,
+                                 date_of_birth=date_of_birth, gender=gender)
         session.add(new_employee)
         session.commit()
 
@@ -73,7 +85,8 @@ def register_employee():
         employee_id = new_employee.id
 
         
-        new_registration = Registration(department=department, position=position, hire_date=hire_date, employee_id=employee_id )
+        new_registration = Registration(department=department, position=position, hire_date=hire_date, status=status,
+                                         employee_id=employee_id)
         session.add(new_registration)
         session.commit()
              
@@ -83,10 +96,30 @@ def register_employee():
     return render_template('registration_form.html')
 
 # A function to view all registered employees
+@app.route('/view')
+def view_employee():
+    session = Session()
+    registered_employees = session.query(Employees, Registration).join(Registration, Employees.id == Registration.employee_id).all()
+    return render_template('view.html', registered_employees=registered_employees)
+
+
+# A function to increase salary based on time on the job
+
 
 # A function to delete employees
 
-# A function to increase salary based on time on the job
+
+# Clears all tables from the database
+@app.route('/clear_database')
+def clear_database():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    print("Database cleared!")
+    return render_template('view.html')
+
+
+
+
 
 
 if __name__ == '__main__':
